6b83ba44f5732b3c656fa5ee7ee3ea715266a976
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-12-23 23:08:57 +0545

    project setup for blog webapplication

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import 'dotenv/config'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import dbConnection from './config/connection.js'
+import globalErrorHandler from './middleware/globalErrorHandling.js'
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+// Security & parsing
+app.use(helmet())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+// Logging
+app.use(morgan('dev'))
+
+// CORS
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
+
+//global error handler
+app.use(globalErrorHandler)
+
+// connect db and start server
+dbConnection().then(() => {
+    app.listen(PORT, () => {
+        console.log(` Server running at http://localhost:${PORT}`)
+    })
+
+})
