1832118e76f495399c2afad8e24311cbecd7a8b7
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-12-31 16:05:27 +0545

    feat(admin): implement delete user by admin functionality

--- backend/controllers/admin/user.controller.js
+++ backend/controllers/admin/user.controller.js
@@ -24,3 +24,49 @@ export const getAllUserController = async (req, res) => {
         })
     }
 }
+
+// Delete user by admin
+export const deleteUserController = async (req, res) => {
+    try {
+        const adminId = req.user._id;
+        const userId = req.params.userId;
+
+        const adminUser = await User.findById(adminId);
+        if (!adminUser || adminUser.role !== 'admin') {
+            return res.status(403).json({
+                success: false,
+                message: 'Access denied. Only admins can delete users.'
+            });
+        }
+
+        if (userId === adminId.toString()) {
+            return res.status(400).json({
+                success: false,
+                message: 'Admins cannot delete themselves'
+            });
+        }
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        }
+
+        await User.findByIdAndDelete(userId);
+
+        return res.status(200).json({
+            success: true,
+            message: 'User deleted successfully by admin',
+            user
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Failed to delete user: server error"
+        });
+    }
+}
--- backend/router/admin/admin.route.js
+++ backend/router/admin/admin.route.js
@@ -1,7 +1,9 @@
 import express from 'express'
-import { getAllUserController } from '../../controllers/admin/user.controller.js'
+import { deleteUserController, getAllUserController } from '../../controllers/admin/user.controller.js'
 import authMiddleware from '../../middleware/auth.js'
 import roleAuthorization from '../../middleware/roleAuthorization.js'
 const adminRouter = express.Router()
 adminRouter.get('/get-all-user', authMiddleware, roleAuthorization('admin'), getAllUserController)
+adminRouter.delete('/delete-user/:userId', authMiddleware, roleAuthorization('admin'), deleteUserController);
+
 export default adminRouter
